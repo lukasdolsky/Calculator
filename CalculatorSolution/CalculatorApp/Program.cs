9a34c5c56ef6948de531886e5dba12a53fad8671
Lukas Dolsky <lukdol@post.cz>
2018-12-02 17:34:21 +0100

    - Added Calculator Project

---
+++ CalculatorSolution/CalculatorApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace CalculatorApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // Spusteni "Windows Forms"aplikace Calculator
+            Application.Run(new Calculator());
+        }
+    }
+}
