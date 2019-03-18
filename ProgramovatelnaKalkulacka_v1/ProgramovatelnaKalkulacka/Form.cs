676d70bc29533891c4eb7f4f3894ed3d5833f782
Lukas Dolsky <lukdol@post.cz>
2019-03-18 23:46:01 +0100

    Added XML support

---
+++ ProgramovatelnaKalkulacka_v1/ProgramovatelnaKalkulacka/Form.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ProgramovatelnaKalkulacka
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+
+            // Nastaveni vysky na 600px pro spravne zobrazeni
+            this.Height = 600;
+        }
+
+
+        #region Vypocet Polynomu
+        // Vypocet Polynomu
+
+        // Preddefinovane hodnoty pro snadnejsi debugging a zaroven pro jednoduche instrukce 
+        private void Form_Load(object sender, EventArgs e)
+        {
+            tbPolynom.Text = "y = x^2 - 3x + 3";
+            tbPocatek.Text = "-5";
+            tbKonec.Text = "5";
+            tbPocet.Text = "10";
+        }
+
+        private DataTable dtVysledek;
+        private void button26_Click(object sender, EventArgs e) {
+            try {
+                this.Cursor = Cursors.WaitCursor;
+
+                if (dtVysledek != null)
+                {
+                    dtVysledek.Clear();
+                    dgBody.Refresh();
+                    grafPolynom.DataBind();
+                    grafPolynom.Update();
+                }
+
+                // Vypocet hodnot zavisle promenne X pro nezavisle promenne Y
+                dtVysledek = Polynom.VypoctiPolynom(tbPolynom.Text.Trim(), Convert.ToDouble(tbPocatek.Text), Convert.ToDouble(tbKonec.Text), Convert.ToInt32(tbPocet.Text));
+                // Vytvoreni dat pro DataGrid
+                dgBody.DataSource = dtVysledek;
+                dgBody.Refresh();
+
+                // Vytvoreni dat pro graf
+                grafPolynom.DataSource = dtVysledek;
+                grafPolynom.DataBind();
+                grafPolynom.Update();
+
+                this.Cursor = Cursors.Default;
+            }
+            catch (Exception ex)
+            {
+                this.Cursor = Cursors.Default;
+                MessageBox.Show(ex.ToString());
+            }
+            string input = "Zadaný polynom: " + tbPolynom.Text.Trim() + " byl vykreslen.";
+            Logging.WriteLog(input, "C:\\temp\\Kalkulacka");
+        }
+        #endregion
+
+        #region Kalkulator
+        private decimal MemoryStore = 0;
+        public decimal vysledek = 0;
+
+        private void btnClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            string vystup;
+            if (Kalkulacka.jeOperator(btn.Text))
+            {
+                vystup = " " + btn.Text + " ";
+            }
+            else
+            {
+                vystup = btn.Text;
+            }
+
+            updateDisplay(vystup);
+
+        }
+
+        private void updateDisplay(string update, Boolean replace = false)
+        {
+            if (tbDisplay.Text == "0" || replace)
+            {
+                tbDisplay.Text = update;
+            }
+            else
+            {
+                tbDisplay.Text += update;
+            }
+
+        }
+
+        private void btnVypocet(object sender, EventArgs e)
+        {
+            tbDisplay.Text = Kalkulacka.vypocitej(tbDisplay.Text);
+        }
+
+        private void smazatVseClick(object sender, EventArgs e)
+        {
+            tbDisplay.Text = Kalkulacka.smazatVse(tbDisplay.Text);
+        }
+
+        private void smazatJedenZnakClick(object sender, EventArgs e)
+        {
+            tbDisplay.Text = Kalkulacka.smazatJedenZnak(tbDisplay.Text);
+        }
+
+        private void odmocnitClick(object sender, EventArgs e)
+        {
+            tbDisplay.Text = Convert.ToString(Math.Sqrt(Convert.ToDouble(tbDisplay.Text)));
+        }
+
+
+        private void MSClick(object sender, EventArgs e)
+        {
+           MemoryStore = Kalkulacka.ulozDoPameti(Convert.ToDecimal(tbDisplay.Text));
+        }
+
+        private void MRClick(object sender, EventArgs e)
+        {
+            tbDisplay.Text = Kalkulacka.ziskejZPameti(Convert.ToString(MemoryStore), tbDisplay.Text);
+        }
+
+        private void MCClick(object sender, EventArgs e)
+        {
+            MemoryStore = 0;
+        }
+        #endregion
+
+        #region Hlasovaci Prava
+        private double plochaCelkem;
+        private double plochaZadanychJednotek;
+        private double plochaVsechJednotek;
+        private XmlDocument doc = new XmlDocument();
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        private void importXMLFileDialog_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "XML|*.xml";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                doc = HlasovaciPrava.nactiXML(ofd.FileName);
+                label6.Text = ofd.FileName;
+                plochaCelkem = HlasovaciPrava.plochaVsechJednotek(doc);
+            }
+        }
+        private void btnJednotky_Click(object sender, EventArgs e)
+        {
+            List<string> bytoveJednotky = HlasovaciPrava.nactiJednotky(tbJednotky.Text);
+            plochaZadanychJednotek = HlasovaciPrava.vypoctiPlochuZadanychJednotek(bytoveJednotky,doc);
+            plochaVsechJednotek = HlasovaciPrava.plochaVsechJednotek(doc);
+            if (HlasovaciPrava.jeUsnasenischopna(plochaVsechJednotek, plochaZadanychJednotek))
+            {
+                label7.ForeColor = System.Drawing.Color.Green;
+                label7.Text = string.Format("Schuze je usnasenischopna, podil zucastnenych vlastniku jednotek je: {0} %", Math.Round(plochaZadanychJednotek / plochaVsechJednotek * 100,2));
+            }
+            else {
+                label7.ForeColor = System.Drawing.Color.Red;
+                label7.Text = string.Format("Schuze neni usnasenischopna, podil zucastnenych vlastniku jednotek je: {0} %", plochaZadanychJednotek / plochaVsechJednotek * 100);
+            }
+        }
+        
+        #endregion
+
+
+    }
+}
---
+++ ProgramovatelnaKalkulacka_v1/ProgramovatelnaKalkulacka/Polynom.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ProgramovatelnaKalkulacka
+{
+    public class Polynom
+    {
+        // Regularni vyraz ziskava jednotlive cleny mnohoclenu vcetne znamenka jednotlivych clenu.
+        private static Regex PolynomSplitterExpression = new Regex(@"y\s*=(?<clen>\s*(?<znamenko>(\+|\-|))(?<cast>[^\+\-]+))+");
+
+        // Regularni vyraz ziskava exponenty jednotlivych clenu, aby mohly byt dale vyuzity
+        private static Regex TermSplitterExpression = new Regex(@"^(?<k_n>[0-9\.]*)\s*(/)?\s*(?<k_d>[0-9]*)\s*(?<x>x{0,1})\s*(\^\s*(?<exp_n>[0-9\.]+)\s*(/)?\s*(?<exp_d>[0-9]*))?$");
+
+        /// <summary>
+        /// Vypocitava zavislou promennou y pro jednotlive nezavisle promenne x 
+        /// </summary>
+        /// <param name="tbPolynom">Polynom ve tvaru: y = ax^n + bx^n-1 + ... + m .</param>
+        /// <param name="tbPocatek">Pocatecni hodnota X.</param>
+        /// <param name="tbKonec">Posledni hodnota X.</param>
+        /// <param name="tbPocet">Pocet hodnot, ktere maji byt vypocitany.</param>
+        /// <returns>Datatable s hodnotami X a Y.</returns>
+
+        public static DataTable VypoctiPolynom(string tbPolynom, double tbPocatek, double tbKonec, int tbPocet)
+        {
+            // Rozdeleni polynomu na cleny
+            Match m = PolynomSplitterExpression.Match(tbPolynom);
+            if (!m.Success)
+                throw new Exception(String.Format("Separace '{0}' na jednotlive cleny nebyla uspesna.", tbPolynom));
+
+            // Vytvoreni Datatable s vysledky
+            DataTable dtVysledek = new DataTable("Vysledky");
+            dtVysledek.Columns.Add("X", typeof(double));
+            dtVysledek.Columns.Add("Y", typeof(double));
+
+            // Vytvoreni Datatable s jednotlivymi cleny
+            DataTable dtCleni = new DataTable("Cleni");
+            dtCleni.Columns.Add("k", typeof(double));
+            dtCleni.Columns.Add("exp", typeof(double));
+
+            double koeficient = 0.0;
+            double exponent = 0.0;
+            int znamenko = 1;
+            int ziskanyIndex = 0; // pro ziskani spravneho znamenka
+            
+            #region Vypocet koeficientu a exponentu pro kazdy clen mnohoclenu
+            foreach (Capture ziskanyClen in m.Groups["cast"].Captures) {
+                Match mClen = TermSplitterExpression.Match(ziskanyClen.Value.Trim());
+                if (!mClen.Success)
+                    throw new Exception(String.Format("Clen '{0}' neni podporovan.", ziskanyClen.Value.Trim()));
+
+                string s = m.Groups["znamenko"].Captures[ziskanyIndex++].Value.Trim();
+                if ((s == "+") || (s == "")) // Pokud je prazdne, predpoklada se kladne znamenko.
+                    koeficient = 1;
+                else if (s == "-")
+                    koeficient = -1;
+
+                // Ziskani hodnoty koeficientu
+                koeficient = znamenko * VypoctiMezivysledek(mClen.Groups["k_n"].Value, mClen.Groups["k_d"].Value);
+
+                // Ziskani hodnoty exponentu
+                if (String.IsNullOrEmpty(mClen.Groups["x"].Value))
+                    exponent = 0.0;
+                else
+                    exponent = VypoctiMezivysledek(mClen.Groups["exp_n"].Value, mClen.Groups["exp_d"].Value);
+
+                // Add the values to the data table
+                dtCleni.Rows.Add(koeficient, exponent);
+            }
+            #endregion
+
+            #region Vypocet hodnoty Y
+            // Vypocet velikosti kroku podle poctu X
+            double velikostKroku = (tbKonec - tbPocatek) / (double)tbPocet;
+            double y = 0.0;
+            for (double x = tbPocatek; x <= tbKonec; x += velikostKroku) {
+                y = 0.0;
+                foreach (DataRow drClen in dtCleni.Rows) {
+                    koeficient = (double)(drClen)["k"];
+                    exponent = (double)(drClen)["exp"];
+
+                    if (exponent == 0.0)
+                        y += koeficient;
+                    else if (exponent == 1)
+                        y += (koeficient * x);
+                    else
+                        y += (koeficient * Math.Pow(x, exponent));
+
+                }
+                dtVysledek.Rows.Add(x, y);
+
+            }
+            return dtVysledek;
+            #endregion
+
+        }
+
+        public static double VypoctiMezivysledek(string _citatel, string _jmenovatel)
+        {
+           // Nastaveni vychozich hodnot
+            double citatel = 1.0;
+            double jmenovatel = 1.0;
+
+            // Ziskej citatele, pokud neni prazdny
+            if (!String.IsNullOrEmpty(_citatel))
+            {
+                citatel = Convert.ToDouble(_citatel);
+
+                // Ziskej jmenovatele, pokud neni prazdny
+                if (!String.IsNullOrEmpty(_jmenovatel))
+                    jmenovatel = Convert.ToDouble(_jmenovatel);
+            }
+
+            // Calculate the fraction and return the value
+            return (citatel / jmenovatel);
+        }
+
+    }
+
+    }
